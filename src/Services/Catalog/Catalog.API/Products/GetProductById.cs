7b86ce545ea6a33af6a0c0e145d7ca41dc6bcede
Bernal Bogantes <ber24b.c@gmail.com>
2025-07-24 21:58:38 -0600

    Finish of stage 6 Catalog.API, Infrastructure, Handler and Endpoint Classes

--- src/Services/Catalog/Catalog.API/Products/GetProductById.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Catalog.API.Products
-{
-    public class GetProductById
-    {
-    }
-}
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -20,7 +20,7 @@ namespace Catalog.API.Products.GetProducts
 
             })
             .WithName("GetProducts")
-            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Get Products")
             .WithDescription("Get Products");
